76f37e6cbe8f08d1b1a4de49eba7dfd29eed93a2
Evan Xu <evanz.xu@gmail.com>
2023-11-21 22:30:39 -0500

    working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,44 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "ThnkrAI Docs",
+      logo: {
+        src: "/public/assets/logo-svg.svg",
+      },
+      customCss: [
+        // Relative path to your custom CSS file
+        "/src/custom.css",
+        "@fontsource/Inter/400.css",
+        "@fontsource/Inter/600.css",
+      ],
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    tailwind(),
+  ],
 });
